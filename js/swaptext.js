3eb0437a8b8318fe1591ea3d99246305e1bd6832
Henrik Paul <henrik.paul@gmail.com>
2016-03-06 14:19:34 +0200

    Fix swaptext according to JSCS

--- js/swaptext.js
+++ js/swaptext.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Swap two pieces of text in an element.
  * <p>
@@ -15,9 +17,9 @@ function swapText(element, options) {
     if (!options) {
         options = {
             speed: 5000,
-            duration: "1s",
-            fadeOutFunction: "cubic-bezier(0.55, 0.06, 0.68, 0.19)",
-            fadeInFunction: "cubic-bezier(0.22, 0.61, 0.36, 1)"
+            duration: '1s',
+            fadeOutFunction: 'cubic-bezier(0.55, 0.06, 0.68, 0.19)',
+            fadeInFunction: 'cubic-bezier(0.22, 0.61, 0.36, 1)'
         };
     }
 
@@ -27,29 +29,29 @@ function swapText(element, options) {
         element.textContent = "Can't find text to swap";
         return;
     }
+
     var splits = text.split('|', 2);
 
     if (!element.dataset.swapIsInitied) {
         element.dataset.swapIsInitied = true;
         element.style.opacity = 1;
-        element.style.transitionProperty = "opacity";
+        element.style.transitionProperty = 'opacity';
         element.style.transitionDuration = options.duration;
         element.textContent = splits[0];
-    }
-
-    else {
-        var swapOut = function() {
+    } else {
+        var swapOut = function () {
             element.textContent = (element.textContent === splits[0]) ? splits[1] : splits[0];
             element.style.transitionTimingFunction = options.fadeInFunction;
             element.style.opacity = 1;
             element.removeEventListener('transitionend', swapOut);
         };
+
         element.addEventListener('transitionend', swapOut);
         element.style.transitionTimingFunction = options.fadeOutFunction;
         element.style.opacity = 0;
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
         swapText(element);
     }, options.speed);
 }
